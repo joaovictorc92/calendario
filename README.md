bca679a71d12d537472b296e448accf996ff4b6b
Victor <victor@localhost.localdomain>
2020-01-20 23:14:25 -0300

    adicionando projeto

---
+++ README.md
@@ -0,0 +1,2 @@
+# calendario
+Projeto dev eficiente
---
+++ src/test/java/br/com/deveficiente/calendario/CalendarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.calendario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
