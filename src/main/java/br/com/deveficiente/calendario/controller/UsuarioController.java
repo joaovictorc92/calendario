b37f8eb5b21e354090ba982881755e2d007ae04f
Victor <victor@localhost.localdomain>
2020-01-21 22:02:35 -0300

    adicionando data cadastro

--- src/main/java/br/com/deveficiente/calendario/controller/UsuarioController.java
+++ src/main/java/br/com/deveficiente/calendario/controller/UsuarioController.java
@@ -5,11 +5,13 @@ import br.com.deveficiente.calendario.model.Usuario;
 import br.com.deveficiente.calendario.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
